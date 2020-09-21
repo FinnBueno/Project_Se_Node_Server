85c2b0b0af6b2f29f45f048bf1635a54497be867
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-09-21 21:31:11 +0200

    added basic user creation should have a meeting discussing how we would like to set this up, apollo is WIP

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const _ = require('lodash');
+const { v4 } = require('uuid');
 
 module.exports = ({ model }) => {
     const router = express.Router();
@@ -7,17 +8,39 @@ module.exports = ({ model }) => {
     const { Deceased } = model;
 
     router.post('/newDeceased', async (req, res) => {
-        var deceasedName = req.body.deceasedName;
+        const uuidDesceased = v4();
 
         try {
             const saved = await Deceased.findOneAndUpdate(
-                { deceasedName },
-                { deceasedName },
+                { uuidDesceased },
+                { uuidDesceased },
                 { upsert: true, new: true }
             );
             res.status(200).json(saved);
         } catch (error) {
             res.status(500).json(error);
+        }
+    });
+
+    router.post('/deceasedData', async (req, res) => {
+        var uuid = req.body.uuid;
+
+        const deceasedData = new Object({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+        });
+        const deceasedPerson = await Deceased.findOne({ uuidDesceased: uuid });
+        console.log(deceasedPerson);
+        if (deceasedPerson) {
+            try {
+                await deceasedPerson.deceasedData.push(deceasedData);
+                const saved = await deceasedPerson.save();
+                res.status(200).json(saved);
+            } catch (error) {
+                res.status(500).json(error);
+            }
+        } else {
+            res.status(404).json('uuid not found');
         }
     });
 
